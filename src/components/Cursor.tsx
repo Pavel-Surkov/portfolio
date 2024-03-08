c54e183f30c0fecfc99d2436db4866524c33a3d0
Pavel-Surkov <surkovv.pavel@gmail.com>
2024-03-08 23:03:03 +0700

    feat: add custom cursor

---
+++ src/components/Cursor.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import { useEffect, useRef, useState } from 'react';
+import { motion } from 'framer-motion';
+
+export default function Cursor() {
+  const cursorRef = useRef<HTMLDivElement>(null);
+
+  const [cursorCoords, setCursorCoords] = useState({ x: 0, y: 0 });
+  const [mouseDown, setMouseDown] = useState(false);
+
+  useEffect(() => {
+    if (!cursorRef.current) return;
+
+    document.addEventListener('mousemove', (e) => {
+      setCursorCoords({ x: e.pageX, y: e.pageY });
+    });
+
+    document.addEventListener('mousedown', () => {
+      setMouseDown(true);
+    });
+
+    document.addEventListener('mouseup', () => {
+      setMouseDown(false);
+    });
+
+    return () => {
+      document.removeEventListener('mousemove', (e) => {
+        setCursorCoords({ x: e.pageX, y: e.pageY });
+      });
+
+      document.removeEventListener('mousedown', () => {
+        setMouseDown(true);
+      });
+
+      document.removeEventListener('mouseup', () => {
+        setMouseDown(false);
+      });
+    };
+  }, [cursorRef]);
+
+  return (
+    <motion.div
+      ref={cursorRef}
+      className="bg-mint pointer-events-none fixed left-0 top-0 z-50 hidden h-4 w-4 select-none rounded-full mix-blend-difference md:inline-block"
+      animate={{
+        x: cursorCoords.x - 8,
+        y: cursorCoords.y - 8,
+        scale: mouseDown ? 1.8 : 1,
+      }}
+      transition={{
+        duration: 0.1,
+        stiffness: 50,
+      }}
+    />
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        suisse: ['var(--font-suisse-intl)'],
+      },
+      colors: {
+        dark: '#211951',
+        violet: '#836FFF',
+        mint: '#15F5BA',
+        mixed: '#e32765',
+        gray: '#F0F3FF',
+        white: '#FFF',
+        black: '#000',
       },
     },
   },
