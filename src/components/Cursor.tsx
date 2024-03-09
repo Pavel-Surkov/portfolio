35e8f27261b9e4213bf3c7c15e54a5eb56dd3dd8
Pavel-Surkov <surkovv.pavel@gmail.com>
2024-03-09 18:01:28 +0700

    replace framer cursor with gsap

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -1,57 +1,71 @@
 'use client';
 
-import { useEffect, useRef, useState } from 'react';
-import { motion } from 'framer-motion';
+import { useEffect, useRef } from 'react';
+import gsap from 'gsap';
 
 export default function Cursor() {
   const cursorRef = useRef<HTMLDivElement>(null);
-
-  const [cursorCoords, setCursorCoords] = useState({ x: 0, y: 0 });
-  const [mouseDown, setMouseDown] = useState(false);
 
   useEffect(() => {
     if (!cursorRef.current) return;
 
     document.addEventListener('mousemove', (e) => {
-      setCursorCoords({ x: e.clientX, y: e.clientY });
+      gsap.to(cursorRef.current, {
+        x: e.x - 8,
+        y: e.y - 8,
+        duration: 0.7,
+        ease: 'power4',
+      });
     });
 
     document.addEventListener('mousedown', () => {
-      setMouseDown(true);
+      gsap.to(cursorRef.current, {
+        scale: 1.8,
+        duration: 0.18,
+        ease: 'power1',
+      });
     });
 
     document.addEventListener('mouseup', () => {
-      setMouseDown(false);
+      gsap.to(cursorRef.current, {
+        scale: 1,
+        duration: 0.18,
+        ease: 'power1',
+      });
     });
 
     return () => {
       document.removeEventListener('mousemove', (e) => {
-        setCursorCoords({ x: e.clientX, y: e.clientY });
+        gsap.to(cursorRef.current, {
+          x: e.x - 8,
+          y: e.y - 8,
+          duration: 0.7,
+          ease: 'power4',
+        });
       });
 
       document.removeEventListener('mousedown', () => {
-        setMouseDown(true);
+        gsap.to(cursorRef.current, {
+          scale: 1.8,
+          duration: 0.18,
+          ease: 'power1',
+        });
       });
 
       document.removeEventListener('mouseup', () => {
-        setMouseDown(false);
+        gsap.to(cursorRef.current, {
+          scale: 1,
+          duration: 0.15,
+          ease: 'power1',
+        });
       });
     };
   }, [cursorRef]);
 
   return (
-    <motion.div
+    <div
       ref={cursorRef}
       className="bg-mint pointer-events-none fixed left-0 top-0 z-50 hidden h-4 w-4 select-none rounded-full mix-blend-difference md:inline-block"
-      animate={{
-        x: cursorCoords.x - 8,
-        y: cursorCoords.y - 8,
-        scale: mouseDown ? 1.8 : 1,
-      }}
-      transition={{
-        duration: 0.1,
-        stiffness: 50,
-      }}
     />
   );
 }
