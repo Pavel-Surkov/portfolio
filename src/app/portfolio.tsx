d94f9c11eedd4c90cd63b7434936a25a935e234e
Pavel-Surkov <surkovv.pavel@gmail.com>
2024-03-10 03:39:59 +0700

    feat: add progress bar to portfolio section scroll

--- src/app/portfolio.tsx
+++ src/app/portfolio.tsx
@@ -5,10 +5,11 @@ import { useLayoutEffect, useRef } from 'react';
 
 export default function Portfolio() {
   const sectionRef = useRef<HTMLElement>(null);
+  const progressRef = useRef<HTMLDivElement>(null);
   const sliderRef = useRef<HTMLDivElement>(null);
 
   useLayoutEffect(() => {
-    if (sliderRef.current && sectionRef.current) {
+    if (sliderRef.current && sectionRef.current && progressRef.current) {
       gsap.registerPlugin(ScrollTrigger);
 
       const mainTimeline = gsap.timeline({
@@ -24,18 +25,36 @@ export default function Portfolio() {
         x: -sliderRef.current.offsetWidth + sectionRef.current.offsetWidth,
         ease: 'none',
       });
+
+      gsap
+        .timeline({
+          scrollTrigger: {
+            trigger: sliderRef.current,
+            start: 'left left',
+            end: 'right right',
+            scrub: true,
+            containerAnimation: mainTimeline,
+          },
+        })
+        .fromTo(progressRef.current, { scaleX: 0 }, { scaleX: 1 });
     }
-  }, [sliderRef, sectionRef]);
+  }, [sliderRef, sectionRef, progressRef]);
 
   return (
     <section
       ref={sectionRef}
-      className="bg-dark border-gray max-w-screen relative z-10 h-[5000px] border-t-2"
+      className="bg-dark max-w-screen relative z-10 h-[5000px]"
     >
-      <div className="sticky top-0 overflow-hidden bg-white">
+      <div className="sticky top-0 flex flex-col overflow-hidden bg-white">
+        <div className="relative h-4">
+          <div
+            ref={progressRef}
+            className="bg-mixed absolute left-0 right-0 top-0 h-full origin-left scale-x-0"
+          />
+        </div>
         <div
           ref={sliderRef}
-          className="container relative flex h-screen w-[5000px] items-stretch gap-10 py-[5vh]"
+          className="container relative flex h-screen w-[5000px] flex-grow items-stretch gap-10 py-[5vh]"
         >
           <div className="vertical relative flex rotate-180 justify-end pl-10 text-[14vh] font-semibold uppercase text-black">
             Portfolio
